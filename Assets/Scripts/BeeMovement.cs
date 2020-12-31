e98f23294af90b216bdd6e138a5630eab2f1f479
antaine <aconn@outlook.com>
2020-12-31 18:30:44 +0000

    Basic Border and Test Movement

---
+++ Assets/Scripts/BeeMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeeMovement : MonoBehaviour
+{
+    Rigidbody2D myRb;
+    [SerializeField] float xSpeed = 2.0f;
+    [SerializeField] float ySpeed = 2.0f;
+    CapsuleCollider2D BeeCollider;
+    //Start is called before the first frame update
+    void Start()
+    {
+        myRb = GetComponent<Rigidbody2D>();
+        BeeCollider = GetComponent<CapsuleCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRb.velocity = new Vector2(xSpeed,ySpeed);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision){     
+       xSpeed *= -1f;
+       ySpeed *= -1f;
+    }
+}
