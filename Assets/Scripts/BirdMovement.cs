6d59c64ea4314b794cd6b8e64ad71413b1127e80
antaine <aconn@outlook.com>
2021-01-01 19:40:26 +0000

    Flower Collision and Bird Movement

--- Assets/Scripts/BirdMovement.cs
+++ Assets/Scripts/BirdMovement.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class BirdMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D myRb;
+    [SerializeField] float xSpeed = 2.0f;
+    [SerializeField] float ySpeed = 2.0f;
+    CapsuleCollider2D BirdCollider;
     void Start()
     {
-        
+        myRb = GetComponent<Rigidbody2D>();
+        BirdCollider = GetComponent<CapsuleCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        myRb.velocity = new Vector2(xSpeed,ySpeed);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision){     
+       xSpeed *= -1f;
+       ySpeed *= -1f;
     }
 }
--- Assets/Scripts/FlowerPickup.cs
+++ Assets/Scripts/FlowerPickup.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class FlowerPickup : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision){
-        if(collision.tag == "Player"){
+        if(collision.tag == "Bees"){
             Destroy(gameObject);
             FlowerSpawning.curFlowers--;
         }
+        print(collision.tag);
     }
 
 }
