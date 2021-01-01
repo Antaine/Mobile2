c040a49874e05f5d517239e2391bf616c6ee89f2
antaine <aconn@outlook.com>
2021-01-01 18:25:03 +0000

    Basic Flower Spawning and Pickup

---
+++ Assets/Scripts/FlowerSpawning.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerSpawning : MonoBehaviour
+{
+    public GameObject flowerPrefab;
+    public GameObject quad;
+    public int numOfFlowers = 5;
+    public static int curFlowers =0;
+    private float screenX,screenY;
+    Vector2 pos;
+
+
+    // Update is called once per frame
+    void Update(){
+        if(curFlowers<numOfFlowers){
+            spawnFlowers();
+        }
+        
+    }
+
+    void spawnFlowers(){
+        BoxCollider2D c = quad.GetComponent<BoxCollider2D>();
+        while(curFlowers<numOfFlowers){
+            screenX = Random.Range(c.bounds.min.x, c.bounds.max.x);
+            screenY = Random.Range(c.bounds.min.y, c.bounds.max.y);
+            pos = new Vector2(screenX, screenY);
+            Instantiate(flowerPrefab, pos, transform.rotation);
+            curFlowers++;
+        }
+    }
+
+
+}
