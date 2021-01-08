e2a585eb27f7da5d61f8d01ea41e8fee0c130bc0
antaine <aconn@outlook.com>
2021-01-08 19:56:39 +0000

    1.0

--- Assets/Scripts/BirdStates/StateResting.cs
+++ Assets/Scripts/BirdStates/StateResting.cs
@@ -5,15 +5,22 @@ using System;
 
 public class StateResting : State
 {
+    private float dis1=0;
+    private float dis2 = 6;
+    private float range = 3f;
     public StateResting(Bird connectedBird) : base(connectedBird)
     {
     }
 
     public override Type UpdateState()
     {
-        bird.isFleeing = false;
         Rest();
         bird.CheckEnergy(bird);
+        ScanBees();
+        if(bird.isChasing)
+        {
+            return typeof(StateChasing);
+        }
         if(!bird.isResting){
             return typeof(StateFlying);
         }
@@ -28,5 +35,28 @@ public class StateResting : State
             bird.isResting = false;
             bird.atNest = false;
         }
+    }
+
+    private void ScanBees(){
+        int i =0;
+        foreach(GameObject bee in SpawnHive.activeBees)
+        {
+            if(bee != null){
+                this.dis1 = Vector2.Distance(bird.transform.position,bee.transform.position);
+                if(this.dis1 < this.dis2){
+                    if(Vector2.Distance(bird.transform.position,bee.transform.position) <= range)
+                    {
+                        this.dis2 = this.dis1;
+                        bird.isChasing = true;
+                        bird.atNest = false;
+                        bird.isResting =false;
+                        bird.targetId = i;
+                    }
+                }      
+            }
+            i++;
+        }
+        this.dis1 = 0;
+        this.dis2 = range+5;
     }
 }
